a95266f54ec0e3ab5282ed070f380ea367637009
Branislav Balaz <branislav.balaz@xit.camp>
2022-01-23 16:28:20 +0100

    final submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    const principalElement = document.getElementById("principal");
+    if (isValidPrincipal()) {
+        const principal = principalElement.value;
+        const rate = document.getElementById("rate").value;
+        const years = document.getElementById("years").value;
+        const interest = principal * years * rate / 100;
+        const year = new Date().getFullYear() + parseInt(years);
+        document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of <mark>${rate}%</mark>.<br>You will receive an amount of <mark>${interest}</mark>,<br>in the year <mark>${year}</mark>`;
+    } else {
+        alert("Enter a positive number");
+        principalElement.focus();
+    }
+}
+
+function updateRate() {
+    document.getElementById("rate_val").innerText = `${document.getElementById("rate").value}%`;
+}
+
+function isValidPrincipal() {
+    var principal = document.getElementById("principal").value;
+    return principal > 0;
 }
         
